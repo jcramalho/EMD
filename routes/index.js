e61aa6cdbdcf9549a9b27666db00a74789697788
José Carlos Ramalho <jcr@iMac-de-iMac.lan>
2020-09-11 22:48:39 +0100

    Adicionei o exportador em CSV da BD.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const EMD = require('../controllers/emd')
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -44,5 +45,37 @@ router.post('/emdAlterado', function(req,res){
     .then(dados => res.render('registoAlterado', {dados: dados}))
     .catch(e => res.render('error', {error: e}))
 })
+
+router.get('/exportCSV', function(req, res){
+  EMD.listar()
+    .then(dados => {
+      var mylista = dados.map(e => {
+        e.dataExame = e.dataExame.replace(" ","");
+        e.dataExame = e.dataExame.substring(6) + '-' + e.dataExame.substring(3,5) + '-' + e.dataExame.substring(0,2);
+        return e;
+      })
+      mylista.sort(function(a, b) {
+        if (a.dataExame < b.dataExame) {
+          return 1;
+        }
+        else
+          return -1;
+      });
+
+      const csvWriter = createCsvWriter({
+        path: 'EMD-export.csv',
+        header: ['id', 'dataExame', 'nome', 'genero', 'dataNasc', 'nif', 'morada', 'federado', 'modalidade', 
+                'clube', 'resultadoEMD', 'exameComplementar', 'controlo', 'pagamento', 'contacto' ]
+      });
+
+      csvWriter.writeRecords(mylista)     
+        .then(() => {
+          res.download('EMD-export.csv', 'EMD-export.csv');
+          console.log('Exportação realizada...');
+        })
+        .catch(erro => res.render('error', {error: erro}))
+    })
+    .catch(e => res.render('error', {error: e}))
+});
 
 module.exports = router;
